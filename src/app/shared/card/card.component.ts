c9cf63597bc323247fb4371e7abf36bd4ecbc523
Aleix Ferré <93319700+AleixFerreCP@users.noreply.github.com>
2024-05-21 23:21:59 +0200

    Fixing weird spacing on closed items list

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -3,17 +3,19 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { Item } from '../../home/items-list/items.model';
 import { CardAction, CardActionColors, CardActionIcons } from './card.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule],
+  imports: [MatButtonModule, MatIconModule, CommonModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
 })
 export class CardComponent {
   @Input() item!: Item;
   @Input() actions!: CardAction[];
+  @Input() isFirst!: boolean;
   @Output() onClick = new EventEmitter<CardAction>();
 
   CardActionIcons = CardActionIcons;
